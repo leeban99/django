70a57994043c6d0e96972900c16bbddef70b94bb
leeban99 <leeban99@gmail.com>
2021-07-11 14:07:01 +0530

    latest updates

--- hello_world/app1/views.py
+++ hello_world/app1/views.py
@@ -7,5 +7,11 @@ def home(request):
     return render(request, 'a.html')
 
 
+def expression(request):
+    a = request.post("text1")
+    return render(request, 'output.html',{'result':"testing...."})
+
+
+
 def profile(request):
     return HttpResponse('<b>profile page</b>')
--- hello_world/hello_world/urls.py
+++ hello_world/hello_world/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app1.urls')),
-    path('profile', include('app1.urls'))
+    path('profile', include('app1.urls')),
+    path('expression', include('app1.urls'))
 ]
